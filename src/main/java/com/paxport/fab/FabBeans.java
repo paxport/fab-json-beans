7fe8d18f69f09524127a0ef6a10080c0e20ceaf2
Adam Chesney <ajchesney@gmail.com>
2016-12-06 19:59:15 +0000

    added Jackson for JSON processing

--- src/main/java/com/paxport/fab/FabBeans.java
+++ src/main/java/com/paxport/fab/FabBeans.java
@@ -1,17 +1,25 @@
 package com.paxport.fab;
 
-import org.eclipse.persistence.jaxb.MarshallerProperties;
-import org.eclipse.persistence.jaxb.UnmarshallerProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationModule;
+import com.paxport.fab.beans.ObjectFactory;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.UnsupportedEncodingException;
+import java.nio.charset.StandardCharsets;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
+import javax.xml.transform.stream.StreamSource;
 
 /**
  * Convert FAB XML into JSON
@@ -19,55 +27,70 @@ import javax.xml.bind.Unmarshaller;
 public class FabBeans {
 
     public final static String JSON_MEDIA_TYPE = "application/json";
-    public final static String XML_MEDIA_TYPE = "application/xml";
+    public final static String XML_MEDIA_TYPE = null;
 
-    public <E> E unmarshal(InputStream source, String mediaType, Class<E> type){
+    private final JAXBContext context = createJaxbContext();
+    private final ObjectMapper mapper = createMapper();
+
+    private static JAXBContext createJaxbContext() {
         try {
-            JAXBContext jc = JAXBContext.newInstance(type);
-            Unmarshaller unmarshaller = jc.createUnmarshaller();
-            if ( mediaType != null ){
-                unmarshaller.setProperty(UnmarshallerProperties.MEDIA_TYPE, mediaType);
-            }
-            unmarshaller.setProperty(UnmarshallerProperties.JSON_INCLUDE_ROOT, true);
-            return (E) unmarshaller.unmarshal(source);
+            return JAXBContext.newInstance(ObjectFactory.class.getPackage().getName());
+        } catch (JAXBException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private ObjectMapper createMapper()
+    {
+        final ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JaxbAnnotationModule());
+        // don't break if our version of model has new things in
+        mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
+
+        // don't bother sending nulls
+        mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
+
+        mapper.enable(SerializationFeature.INDENT_OUTPUT);
+        return mapper;
+    }
+
+    public <E> E fromXML(InputStream source, Class<E> type){
+        try {
+            Unmarshaller unmarshaller = context.createUnmarshaller();
+            return unmarshaller.unmarshal(new StreamSource(source), type).getValue();
         } catch (JAXBException e) {
             throw new RuntimeException("Failed to unmarshal into type: " + type.getName(),e);
         }
     }
 
-    public <E> E fromXML(InputStream source, Class<E> type){
-        return unmarshal(source,XML_MEDIA_TYPE,type);
+    public <E> E fromXML(String xml, Class<E> type){
+        return fromXML(new ByteArrayInputStream(xml.getBytes(StandardCharsets.UTF_8)),type);
     }
 
     public <E> E fromJSON(InputStream source, Class<E> type){
-        return unmarshal(source,JSON_MEDIA_TYPE,type);
+        try {
+            return mapper.readValue(source,type);
+        } catch (IOException e) {
+            throw new RuntimeException("failed to read json into " + type.getName(),e);
+        }
     }
 
-    public void marshal (Object bean, OutputStream out, String mediaType, boolean prettyPrint){
+    public <E> E fromJSON(String json, Class<E> type){
+        return fromJSON(new ByteArrayInputStream(json.getBytes(StandardCharsets.UTF_8)),type);
+    }
+
+    public void toXML(Object bean, OutputStream out, boolean prettyPrint){
         try {
-            // Create a JaxBContext
-            JAXBContext jc = JAXBContext.newInstance(bean.getClass());
-
             // Create the Marshaller Object using the JaxB Context
-            Marshaller marshaller = jc.createMarshaller();
-
-            // Set the Marshaller media type to JSON or XML
-            marshaller.setProperty(MarshallerProperties.MEDIA_TYPE,mediaType);
-
-            // Set it to true if you need to include the JSON root element in the JSON output
-            marshaller.setProperty(MarshallerProperties.JSON_INCLUDE_ROOT, true);
+            Marshaller marshaller = context.createMarshaller();
 
             // Set it to true if you need the JSON output to formatted
             marshaller.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, prettyPrint);
 
-            marshaller.marshal(jc, out);
+            marshaller.marshal(bean, out);
         } catch (JAXBException e) {
             throw new RuntimeException("Failed to marshal from type: " + bean.getClass().getName(),e);
         }
-    }
-
-    public void toXML(Object bean, OutputStream out, boolean prettyPrint){
-        marshal(bean,out,XML_MEDIA_TYPE,prettyPrint);
     }
 
     public String toXML(Object bean, boolean prettyPrint){
@@ -80,18 +103,42 @@ public class FabBeans {
         }
     }
 
-    public void toJSON(Object bean, OutputStream out, boolean prettyPrint){
-        marshal(bean,out,JSON_MEDIA_TYPE,prettyPrint);
+    public void toJSON(Object bean, OutputStream out){
+        try {
+            mapper.writeValue(out,bean);
+        } catch (IOException e) {
+            throw new RuntimeException("failed to write json",e);
+        }
     }
 
-    public String toJSON(Object bean, boolean prettyPrint){
+    public String toJSON(Object bean){
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        toJSON(bean,out,prettyPrint);
+        toJSON(bean,out);
         try {
             return out.toString("UTF-8");
         } catch (UnsupportedEncodingException e) {
             throw new RuntimeException(e);
         }
+    }
+
+    public <E> void xmlToJson(InputStream source, OutputStream out, Class<E> type, boolean prettyPrint){
+        E bean = fromXML(source,type);
+        toJSON(bean,out);
+    }
+
+    public <E> String xmlToJson(InputStream source, Class<E> type, boolean prettyPrint){
+        E bean = fromXML(source,type);
+        return toJSON(bean);
+    }
+
+    public <E> void jsonToXml(InputStream source, OutputStream out, Class<E> type, boolean prettyPrint){
+        E bean = fromJSON(source,type);
+        toXML(bean,out,prettyPrint);
+    }
+
+    public <E> String jsonToXml(InputStream source, Class<E> type, boolean prettyPrint){
+        E bean = fromJSON(source,type);
+        return toXML(bean,prettyPrint);
     }
 
 }
--- src/test/java/com/paxport/fab/FabBeansTest.java
+++ src/test/java/com/paxport/fab/FabBeansTest.java
@@ -1,7 +1,58 @@
 package com.paxport.fab;
 
+import com.paxport.fab.beans.Basket;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.io.InputStream;
+
 /**
  * Created by ajchesney on 06/12/2016.
  */
 public class FabBeansTest {
+
+    private FabBeans beans = new FabBeans();
+
+    @Test
+    public void fromXMLTest(){
+
+        InputStream xml = FabBeansTest.class.getResourceAsStream("/basket1.xml");
+
+        Basket basket = beans.fromXML(xml,Basket.class);
+
+        Assert.assertNotNull(basket);
+
+        Assert.assertEquals("si3993",basket.getItinerary().getItineraryId());
+    }
+
+    @Test
+    public void fromJSONTest(){
+
+        InputStream xml = FabBeansTest.class.getResourceAsStream("/basket1.json");
+
+        Basket basket = beans.fromJSON(xml,Basket.class);
+
+        Assert.assertNotNull(basket);
+        Assert.assertNotNull(basket.getItinerary());
+        Assert.assertEquals("si3993",basket.getItinerary().getItineraryId());
+    }
+
+
+    @Test
+    public void xmlToJsonTest(){
+
+        InputStream xml = FabBeansTest.class.getResourceAsStream("/basket1.xml");
+
+        String json = beans.xmlToJson(xml,Basket.class,true);
+
+        System.out.println (json);
+
+        Basket basket = beans.fromJSON(json,Basket.class);
+
+        Assert.assertNotNull(basket);
+
+        Assert.assertEquals("si3993",basket.getItinerary().getItineraryId());
+    }
+
 }
